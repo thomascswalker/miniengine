88f61cbdcf3d229c4a7e563968d8522cf26567d1
thomascswalker <thomascswalker@gmail.com>
2022-08-10 16:05:23 -0400

    Reworked to be more effective

---
+++ MiniEngine/MiniEngine/core.h
@@ -0,0 +1,53 @@
+#ifndef CORE_H
+#define CORE_H
+#endif
+
+#ifndef MAIN_WINDOW_TIMER_ID
+    #define MAIN_WINDOW_TIMER_ID 1001
+#endif
+
+class MSize
+{
+public:
+    MSize(int width, int height) : _width(width), _height(height) {};
+
+    int width() {return _width;}
+    int height() {return _height;}
+    int setWidth(int width) {_width = width;}
+    int setHeight(int height) {_height = height;}
+
+private:
+    int _width;
+    int _height;
+};
+
+int clamp(int *value, int min, int max)
+{
+    if (*value < min)
+    {
+        return min;
+    }
+    if (*value > max)
+    {
+        return max;
+    }
+    return *value;
+}
+
+int getRefreshRate()
+{
+    DEVMODE screen;
+    memset(&screen, 0, sizeof(DEVMODE));
+    int refreshRate = screen.dmDisplayFrequency;
+    return refreshRate;
+}
+
+MSize getScreenSize()
+{
+    RECT desktop;
+    const HWND hDesktop = GetDesktopWindow();
+
+    GetWindowRect(hDesktop, &desktop);
+
+    return MSize(desktop.right, desktop.bottom);
+}
